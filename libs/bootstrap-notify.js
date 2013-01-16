090279cbb13b7f0980e943b7fe633f72b2ec1f21
Ruben Stolk <ruben.stolk@changer.nl>
2013-01-16 23:41:48 +0530

    Fixed jslint issues

--- libs/bootstrap-notify.js
+++ libs/bootstrap-notify.js
@@ -3,14 +3,14 @@
  * --
   * Copyright 2012 Goodybag, Inc.
  * --
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Apache License, Version 2.0 (the 'License');
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an 'AS IS' BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
@@ -24,34 +24,50 @@
     this.options  = $.extend(true, {}, $.fn.notify.defaults, options);
 
     // Setup from options
-    if(this.options.transition)
-      if(this.options.transition == 'fade')
+    if(this.options.transition) {
+      if(this.options.transition === 'fade') {
         this.$note.addClass('in').addClass(this.options.transition);
-      else this.$note.addClass(this.options.transition);
-    else this.$note.addClass('fade').addClass('in');
+      }
+      else {
+        this.$note.addClass(this.options.transition);
+      }
+    }
+    else {
+      this.$note.addClass('fade').addClass('in');
+    }
 
-    if(this.options.type)
+    if(this.options.type) {
       this.$note.addClass('alert-' + this.options.type);
-    else this.$note.addClass('alert-success');
+    }
+    else {
+      this.$note.addClass('alert-success');
+    }
 
-    if(!this.options.message && this.$element.data("message") !== '') // dom text
-      this.$note.html(this.$element.data("message"));
-    else
-      if(typeof this.options.message === 'object')
-        if(this.options.message.html)
+    if(!this.options.message && this.$element.data('message') !== '') {// dom text
+      this.$note.html(this.$element.data('message'));
+    }
+    else {
+      if(typeof this.options.message === 'object') {
+        if(this.options.message.html) {
           this.$note.html(this.options.message.html);
-        else if(this.options.message.text)
+        }
+        else if(this.options.message.text) {
           this.$note.text(this.options.message.text);
-      else
+        }
+      }
+      else {
         this.$note.html(this.options.message);
+      }
+    }
 
-    if(this.options.closable)
+    if(this.options.closable) {
       var link = $('<a class="close pull-right" href="#">&times;</a>');
       $(link).on('click', $.proxy(onClose, this));
       this.$note.prepend(link);
+    }
 
     return this;
-  };
+  },
 
   onClose = function() {
     this.options.onClose();
